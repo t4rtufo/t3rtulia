a04aa91dff7a7cace16ea6b8140ee20805139c67
t4rtufo <walt2001@icloud.com>
2021-12-20 13:05:51 -0500

    concurrent server added

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const morgan = require('morgan');
+const path = require('path');
+
+//Settings
+const app = express();
+app.set('port', process.env.port || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+	res.sendFile(path.join(__dirname,'view', 'index.html'));
+});
+
+//Excecuting server
+app.listen(app.get('port'), () => {
+	console.log(`Server on port ${app.get('port')}`);
+});
