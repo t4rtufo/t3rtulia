7d6e1658d7d4d4670986c76f43ed7bb148fdbc89
t4rtufo <walt2001@icloud.com>
2021-12-20 13:07:59 -0500

    port updated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 //Settings
 const app = express();
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.port || 80);
 
 //Middlewares
 app.use(morgan('dev'));
